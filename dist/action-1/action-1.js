bf8b68fc60039541d7097a98b8781902abb4cecb
Nipun Harjai <nipunharjai@Nipuns-MacBook-Air.local>
2023-01-21 20:21:59 +0530

    fourth release

---
+++ dist/action-1/action-1.js
@@ -0,0 +1,5 @@
+"use strict";
+const action1 = async () => {
+    console.log("Action 1");
+};
+action1();
---
+++ dist/action-2/action-2.js
@@ -0,0 +1,6 @@
+"use strict";
+const action2 = async () => {
+    console.log("Action 2");
+    console.log("Done");
+};
+action2();
